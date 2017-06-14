934d1e16ead778fab4e987adf371b2ded7f8e054
Osvaldo Villagomez <ovillagomez@jimmyjohns.com>
2017-06-14 11:42:13 -0500

    added lighthouse

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Load Dependencies
 var SitemapGenerator  = require('sitemap-generator');
 var parseString       = require('xml2js').parseString;
+const lighthouse        = require('lighthouse');
+const chromeLauncher    = require('lighthouse/chrome-launcher/chrome-launcher');
+const flags = {output: 'json'};
+
+function launchChromeAndRunLighthouse(url, flags, config = null) {
+  return chromeLauncher.launch().then(chrome => {
+    flags.port = chrome.port;
+    return lighthouse(url, flags, config).then(results =>
+      chrome.kill().then(() => results)
+    );
+  });
+}
 
 // Create Site Generator to Obtain all Site URLs
 var generator = new SitemapGenerator('https://www.jimmyjohns.com');
@@ -12,7 +24,11 @@ generator.on('done', function (sitemaps) {
     var urlset = result.urlset.url; // array on objects containing the loc/url
     urlset.forEach(function(item) {
       var currentURL = item.loc[0];
-      
+      // Use lighthouse on the URL
+      launchChromeAndRunLighthouse(currentURL, flags).then(results => {
+        // Use results!
+        console.log(results);
+      });
     });
   });
 });
