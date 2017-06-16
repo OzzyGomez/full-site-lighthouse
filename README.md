4b419f36312faa2fa30249416d846796b2155096
Osvaldo Villagomez <ovillagomez@jimmyjohns.com>
2017-06-16 11:55:22 -0500

    run via index.js; bug fixes;

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # full-site-lighthouse
 
 An automated way to run Google lighthouse on an entire site
+The script generates a sitemap of your site and runs Google Lighthouse on them.
+
+WARNING: If sitemap if long, script may take a long time to run as it must evaluate each site via Google Lighthouse Asynchronously
+
+## Requirements
+- NodeJS
+
+## To Run
+#### Install Dependencies (Required before first run)
+```shell
+npm install
+```
+
+#### Run Script
+```shell
+node index.js [LANDING PAGE URL]
+```
+include http:// or https://
+
+## Outputs
+Outputs will be in the /output/ directory
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const sitemap = require('./lib/sitemap.js');
-const chrome = require('./lib/chrome.js');
+// Load Dependencies
+var validator   = require('validator');
+var path        = require('path');
 
-sitemap('http://www.jimmyjohns.com', function () {
-  chrome();
-});
+// Custom Modules
+const sitemap   = require('./lib/sitemap.js');
+const chrome    = require('./lib/chrome.js');
+
+if (process.argv[2]) {
+  if (validator.isURL(process.argv[2])) {
+    console.log("Generating Sitemap...");
+    sitemap(process.argv[2], function () {
+      console.log("Generating Audit...");
+      chrome();
+    });
+  } else {
+      console.error(process.argv[2] + " is not a valid URL, please use the full URL.");
+  }
+} else {
+  console.log("\nUsage: node " + path.basename(__filename) + " [URL]\n");
+}
