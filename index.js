9975d186f6738d6ded1031554e0003fcab67a58d
Osvaldo Villagomez <ovillagomez@jimmyjohns.com>
2017-06-23 12:09:51 -0500

    almost finished; html page creates; directory creates

--- index.js
+++ index.js
@@ -2,21 +2,32 @@
 require('babel-register');
 var validator   = require('validator');
 var path        = require('path');
+var fs          = require('fs');
 
 // Custom Modules
 const sitemap   = require('./lib/sitemap.js');
 const chrome    = require('./lib/chrome.js');
 
+var jsonDate, timeInMs, dir;
+
 // Check if Argument exists and is Valid URL
 if (process.argv[2]) {
   if (validator.isURL(process.argv[2])) {
+    //jsonDate = (new Date()).toJSON();
+    timeInMs = Date.now();
+    jsonDate = timeInMs;
+    dir = './output/'+jsonDate+'/';
+    // Create Directory
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
     // If true, call sitemap module to generate sitemap
-    console.log("Generating Sitemap...");
-    sitemap(process.argv[2], function (data) {
+    console.log("Generating Sitemap...\n");
+    sitemap(process.argv[2], jsonDate, function (data) {
       // Once sitemap has been created, Perform Full Site Audit
       console.log("Generating Audit...\n");
       console.log("Depending on the amount of pages you have, this may take a while.\n");
-      chrome(data);
+      chrome(data, jsonDate);
     });
   } else {
     // Error: Url is not valid
